8ba95d22851125e9d048e88eb7756b5775593950
Johnny Ray Austin <johnny@johnnyray.me>
2016-01-08 12:02:29 -0500

    tweaks and bug fixes

--- {{cookiecutter.repo_name}}/app/index.js
+++ {{cookiecutter.repo_name}}/app/index.js
@@ -2,27 +2,31 @@
 'use strict';
 
 {% if cookiecutter.use_express == 'y' -%}
-const express     = require( 'express' );
-const bodyParser  = require( 'body-parser' );
-const swig        = require( 'swig' );
-const app         = express();
+const express = require('express');
+const bodyParser = require('body-parser');
+const nunjucks = require('nunjucks');
+const app = express();
 
-app.use( bodyParser.json() );
-app.use( bodyParser.urlencoded({ extended: true }) );
-app.engine( 'html', swig.renderFile );
-app.set( 'view engine', 'html' );
-app.set( 'view cache', false );
-app.set( 'secret', process.env.SECRET );
+nunjucks.configure('views', {
+  autoescape: true,
+  express: app
+});
 
-app.get('/', ( req, res ) => {
-  res.send( 'I believe in you!' );
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.set('view engine', 'html');
+app.set('view cache', false);
+app.set('secret', process.env.SECRET);
+
+app.get('/', (req, res) => {
+  res.send('I believe in you!');
 });
 
 const server  = app.listen(3000, () => {
   const host    = server.address().address;
   const port    = server.address().port;
-  console.log( 'App listening at http://%s:%s', host, port );
+  console.log('App listening at http://%s:%s', host, port);
 });
 {%- else %}
-console.log( '\n     I believe in you!\n' );
+console.log('\n     I believe in you!\n');
 {%- endif %}
