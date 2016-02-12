a8e3094c95e81fa8b5a46e0f510ef9e7859a4c9b
Johnny Ray Austin <johnny@johnnyray.me>
2016-02-12 11:28:27 -0500

    initial support for npmrc

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -1,6 +1,10 @@
 
 import subprocess
 
+{% raw %}
+with open(".npmrc", "w") as text_file:
+    text_file.write("//registry.npmjs.org/:_authToken=${{{0}}}".format("NPM_TOKEN"))
+{% endraw %}
 subprocess.call(["npm", "install"])
 subprocess.call(["cp", "env.example", ".env"])
 subprocess.call(["nvm", "install"])
